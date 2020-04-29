9f1db8096573d81b7b23f5da47e10b086785da45
hansen <375161864@qq.com>
2020-04-29 14:38:25 +0800

    修复sql的异常编码 增加服务控制sh 增加docker-compose.yml

--- pro/bin/start.sh
+++ pro/bin/start.sh
@@ -5,12 +5,15 @@ if [ ! -z "$isExist" ]; then
     exit -1
 fi
 PRGDIR=`dirname "$0"`
-BASEDIR=`cd "$PRGDIR" ; pwd`
-cd ../
+BASEDIR=`cd "$PRGDIR" ; cd ../runtime ; pwd`
+
 #nohup java -Xms512m -Xmx512m -jar wx-api.jar > log/out.log 2>& 1 &
 
 #nohup java -Xms512m -Xmx512m -jar wx-api.jar > log/out.log 2>& 1
 
-nohup java -Xms512m -Xmx512m -jar wx-api.jar
+
+CLASSPATH=$BASEDIR/config:$BASEDIR/lib/*
+nohup java  -Xmx2g -Xms2g -classpath "$CLASSPATH" \
+                com.github.niefy.BootApplication \
 
 echo $! > progressId.pid
